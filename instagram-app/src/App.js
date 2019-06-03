34866aaad7a82d867c797a38cd6baf85c8a88dbd
JaySebagh <jaysebagh@gmail.com>
2019-06-03 14:30:15 -0700

    rendered posts and styling

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './App.css';
 
+import dummyData from './dummy-data'
+
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <SearchBar />
-      <PostContainer />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: dummyData,
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+        {this.state.data.map(post => {
+          return <PostContainer post={post}/>
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import './PostContainer.css'
 
 import CommentSection from '../CommentSection/CommentSection';
 
-const PostContainer = () => {
+const PostContainer = props => {
     return (
-        <div>
-            PostContainer
-            <CommentSection />
+        <div className="post">
+            <div className="user-container">
+                <h1 className="username"><strong>{props.post.username}</strong></h1>
+                <img className="img-pfp" src={props.post.thumbnailUrl} alt="thumb" />
+            </div>
+
+            <div className="post-container">
+                <img className="img-post" src={props.post.imageUrl} alt="thumb" />
+                <div className="likes-comments">
+                    <p className="likes"><strong>{props.post.likes} likes</strong></p>
+                    <CommentSection />
+                </div>
+            </div>
         </div>
     )
 }
