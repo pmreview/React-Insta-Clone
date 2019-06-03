f6d0353d5da8d40f1fcbcc084a8b8cfc905ed780
JaySebagh <jaysebagh@gmail.com>
2019-06-03 13:37:23 -0700

    scaffolded

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
+
 function App() {
   return (
     <div className="App">
-      hi
+      <SearchBar />
+      <PostContainer />
     </div>
   );
 }
---
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import CommentSection from '../CommentSection/CommentSection';
+
+const PostContainer = () => {
+    return (
+        <div>
+            PostContainer
+            <CommentSection />
+        </div>
+    )
+}
+
+export default PostContainer;
